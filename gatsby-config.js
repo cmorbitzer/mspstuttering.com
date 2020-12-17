34230073e07540625e0918128fc53a49cffd9f3a
Chris Morbitzer <chris.morbitzer@gmail.com>
2020-12-16 19:17:57 -0600

    Start

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,45 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  siteMetadata: {
+    title: `MSP Stuttering Group`,
+    description: `Therapy for all ages and communication goals via in-home or virtual sessions.`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-anchor-links`,
+      options: {
+        offset: -50,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `MSP Stuttering Group`,
+        short_name: `MSP Stuttering`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        icon: `src/images/msp_stuttering_group-logo.png`,
+      },
+    },
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-react-svg`,
+      options: {
+        rule: {
+          include: `${__dirname}/src/icons`,
+        },
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+  ],
+};
